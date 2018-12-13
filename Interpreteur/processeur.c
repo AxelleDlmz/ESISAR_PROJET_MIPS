7dba5edce751aef6fa855cf63e3b1afb9c36bb1a
axelleDlmz <axelledelomez@gmail.com>
2018-12-13 23:10:52 +0100

    int to string en binaire

--- Interpreteur/processeur.c
+++ Interpreteur/processeur.c
@@ -4,6 +4,20 @@
 #include "processeur.h"
 #include "memoire.h"
 
+char * int2StrBinaire(int donnee){
+	char binaire[32] = "";
+	for(int i = 0; i > 32; i++){
+		if(donnee-pow(2,i)>0){
+			strcat(binaire,"1");
+			donnee-=pow(2,i);
+		}else{
+			strcat(binaire,"0");
+		}
+	}
+	return binaire;
+}
+
 instruction decoderInstruction (int donnee){
+	char binaire[32] = int2StrBinaire(donnee);
 	
 }
