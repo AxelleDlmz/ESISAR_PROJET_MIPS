4aacbafcd8034d2ad5c002b63019f4daaf6b01e2
axelleDlmz <axelledelomez@gmail.com>
2018-12-19 16:07:41 +0100

    decode ++

--- Interpreteur/processeur.c
+++ Interpreteur/processeur.c
@@ -4,6 +4,14 @@
 #include "processeur.h"
 #include "memoire.h"
 
+/*	FONCTIONNALITES DU PROCESSEUR
+	
+		- Lire en mémoire à la case du PC
+		- Décoder les instructions
+		- Exécuter les instructions
+		- Ecrire les résultats dans les registres
+*/
+
 char * int2StrBinaire(int donnee){
 	char binaire[32] = "";
 	for(int i = 0; i > 32; i++){
@@ -17,7 +25,79 @@ char * int2StrBinaire(int donnee){
 	return binaire;
 }
 
+/*
+	Récupère le int de l'instruction stocké dans la mémoire
+	Renvoie une instruction exécutable
+*/
 instruction decoderInstruction (int donnee){
+	instruction res;
 	char binaire[32] = int2StrBinaire(donnee);
-	
+	char code[6]="";
+	for(int i = 0; i < 7; i++)
+		code[i]=binaire[i];
+
+	if(code=="000000"){ 		// instruction spéciale
+		for(int i = 26; i < 32; i++)
+			code[i-26]=binaire[i];
+		
+		switch(code){
+			case "100000": // ADD
+				(*res).operande1 = ;
+				(*res).operande2 = ;
+				(*res).retour = ;
+				(*res).operateur = "ADD";
+				break;
+
+			case "100100": // AND
+				(*res).operande1 = ;
+				(*res).operande2 = ;
+				(*res).retour = ;
+				(*res).operateur = "AND" ;
+				break;
+
+			case "011010": // DIV
+				(*res).operande1 = ;
+				(*res).operande2 = ;
+				(*res).retour = ;
+				(*res).operateur = "DIV";
+				break;
+
+			case "001000": // JR
+				(*res).operande1 = ;
+				(*res).operande2 = ;
+				(*res).retour = ;
+				(*res).operateur = "JR";
+				break;
+
+			case "010000": // MFHI
+				(*res).operande1 = ;
+				(*res).operande2 = ;
+				(*res).retour = ;
+				(*res).operateur = "MFHI";
+				break;
+
+			case "010010": // MFLO
+				(*res).operande1 = ;
+				(*res).operande2 = ;
+				(*res).retour = ;
+				(*res).operateur = "MFLO" ;
+				break;
+
+			case "011000": // MULT
+				(*res).operande1 = ;
+				(*res).operande2 = ;
+				(*res).retour = ;
+				(*res).operateur = "MULT";
+				break;
+
+			case "100101": // OR
+				(*res).operande1 = ;
+				(*res).operande2 = ;
+				(*res).retour = ;
+				(*res).operateur = "OR";
+				break;
+		}
+	}else{
+
+	}
 }
